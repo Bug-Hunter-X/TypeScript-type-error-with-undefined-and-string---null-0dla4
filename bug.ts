01f85f96ef18ddeda67b3078b8fab977cdda051b
BugHunter-X <askzenai@gmail.com>
2024-12-12 05:08:05 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,10 @@
+function printName(name: string | null): void {
+  if (name) {
+    console.log(name.toUpperCase());
+  } else {
+    console.log('Name is null');
+  }
+}
+
+printName(null); // This works fine
+printName(undefined); // This will throw an error because undefined is not assignable to string | null
